95b89f234096bc925ccd4607655fe72d8ed9af67
tesseract241 <riccardomastronardi@MacBook-di-Riccardo.local>
2017-08-06 19:02:16 +0200

    reg

--- include/MPL3115A2.h
+++ include/MPL3115A2.h
@@ -3,32 +3,29 @@
 #include "MPL3115A2_enum.h"
 #include "mbed.h"
 
-class MPL3115A2{
-public:
-MPL3115A2(MPL3315A2_Mode mode = BAROMETER, MPL3315A2_Os_Ratio ratio = OS_6_MS);
-double getData();
-double getTemperature();
-double getDataDelta();
-double getTemperatureDelta();
-double getMaxData();
-double getMaxTemperature();
-double getMinData();
-double getMinTemperature();
-int getOsR();
-void setOsR(MPL3315A2_Os_Ratio ratio);
-bool isAltimeter();
-void setMode(MPL3315A2_Mode mode);
+class MPL3115A2
+{
+    public:
+    MPL3115A2(MPL3315A2_Mode mode = BAROMETER, MPL3315A2_Os_Ratio ratio = OS_6_MS);
+    double getData();
+    double getTemperature();
+    double getDataDelta();
+    double getTemperatureDelta();
+    double getMaxData();
+    double getMaxTemperature();
+    double getMinData();
+    double getMinTemperature();
+    int getOsR();
+    void setOsR(MPL3315A2_Os_Ratio ratio);
+    bool isAltimeter();
+    void setMode(MPL3315A2_Mode mode);
 
-private:
+    private:
 
-I2C mI2C;
-uint8_t mAddress;
-void read(MPL3115A2_Address address, uint8_t *data, int length);
-int write(MPL3115A2_Address address, uint8_t *data);
-
-
-
-
+    I2C mI2C;
+    uint8_t mAddress;
+    void read(MPL3115A2_Address address, uint8_t *data, int length);
+    int write(MPL3115A2_Address address, uint8_t *data);
 };
 
 #endif
--- sources/main.cpp
+++ sources/main.cpp
@@ -1,14 +1,14 @@
-#include "mbeh.h"
+#include "mbed.h"
 #include "MPL3115A2.h"
 
 int main(){
-    MPL3115A2 sensor();
+    MPL3115A2 sensor;
     Serial pc(USBTX, USBRX);
     double pressure;
     while(1){
         pressure = sensor.getData();
         pc.baud(19200);
-        pc.prinf("Pressure =%f", pressure);
-        Thread::wait(250;)
+        pc.printf("Pressure = %f\n", pressure);
+        Thread::wait(500);
     }
 }
