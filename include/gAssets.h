fd8c3d7037682feb81f14bc2ae21d57c5a3f9b79
tesseract241 <riccardomastronardi@MacBook-di-Riccardo.local>
2018-06-08 15:26:27 +0200

    Forgot to add the header file for the graphical assets. It contains the definition of the gfx namespace, which holds all of them.

---
+++ include/gAssets.h
@@ -0,0 +1,15 @@
+#ifndef _GASSETS_
+#define _GASSETS_
+#include "mbed.h"
+
+namespace gfx{
+    extern const uint16_t cloudy[];
+    extern const uint16_t heart[];
+    extern const uint16_t humidity[];
+    extern const uint16_t rain[];
+    extern const uint16_t storm[];
+    extern const uint16_t sunny[];
+    extern const uint16_t thermometer[];
+}
+
+#endif
