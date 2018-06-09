af3c668bb82ecd1b238d4c543a4eeaf082c3396b
tesseract241 <riccardomastronardi@MacBook-di-Riccardo.local>
2018-06-09 12:42:39 +0200

    Made some changes to the graphical assets and renamed one.

--- include/gAssets.h
+++ include/gAssets.h
@@ -6,7 +6,7 @@ namespace gfx{
     extern const uint16_t cloudy[];
     extern const uint16_t heart[];
     extern const uint16_t humidity[];
-    extern const uint16_t rain[];
+    extern const uint16_t rainy[];
     extern const uint16_t storm[];
     extern const uint16_t sunny[];
     extern const uint16_t thermometer[];
